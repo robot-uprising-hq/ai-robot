80f79353abd264588694746abbace60c580f7f44
Sami J. Lehtinen <sjl@iki.fi>
2020-09-13 14:42:54 +0300

    Revert "Motor pins DIR_A and DIR_B for both motors swapped to make the robot go right with AI Backend Connector's commands. Previously forwards was backwards"
    This reverts commit 5dd6d112d33daf08744cbc983cb21d0f738695ee.

--- components/motor_control/motor_control.c
+++ components/motor_control/motor_control.c
@@ -13,12 +13,12 @@
 #include "stdio.h"
 
 // Pin definitions
-#define MOTOR_1_DIR_A   23
-#define MOTOR_1_DIR_B   21
+#define MOTOR_1_DIR_A   21
+#define MOTOR_1_DIR_B   23
 #define MOTOR_1_PWM     19
 
-#define MOTOR_2_DIR_A   33
-#define MOTOR_2_DIR_B   25
+#define MOTOR_2_DIR_A   25
+#define MOTOR_2_DIR_B   33
 #define MOTOR_2_PWM     32
 
 #define MOTOR_DIR_PIN_SEL ((1ULL<<MOTOR_1_DIR_A) | (1ULL<<MOTOR_1_DIR_B) | (1ULL<<MOTOR_2_DIR_A) | (1ULL<<MOTOR_2_DIR_B))
